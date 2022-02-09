c0e0a0b664066ac38351e79eeaf5a1dde92c4ca5
Ibrahim Zahid <P2644952@my365.dmu.ac.uk>
2022-02-09 11:52:58 +0000

    helloworldchange

--- templab19/Program.cs
+++ templab19/Program.cs
@@ -10,6 +10,8 @@ namespace templab19
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello worldddd");
+            Console.ReadLine();
         }
     }
 }
